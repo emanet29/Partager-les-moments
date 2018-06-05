4a9b0ac41d06dead343fa085d0d36f19d43d1504
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-05 11:04:51 +0200

    button bar item + text view

---
+++ Partager les moments/PhotoController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by Snoopy on 05/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+    
+    @IBOutlet weak var partagerBouton: UIBarButtonItem!
+    
+    @IBOutlet weak var photoAPartager: UIImageView!
+    
+    @IBOutlet weak var texteAPartager: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func boutonPartagerAppuye(_ sender: Any) {
+    }
+    
+    
+}
