1639ef39892247b86e5da7f30c9e59e53d4e43d3
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-05 11:10:22 +0200

    UITapGesture...

--- Partager les moments/PhotoController.swift
+++ Partager les moments/PhotoController.swift
@@ -11,17 +11,32 @@ import UIKit
 class PhotoController: UIViewController {
     
     @IBOutlet weak var partagerBouton: UIBarButtonItem!
-    
     @IBOutlet weak var photoAPartager: UIImageView!
-    
     @IBOutlet weak var texteAPartager: UITextView!
     
-    
+    let texteVide = "Entrez un texte ..."
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        miseEnPlace()
+    }
+    
+    func miseEnPlace() {
+        photoAPartager.contentMode = .scaleAspectFit
+        photoAPartager.image = #imageLiteral(resourceName: "Superman-facebook.svg")
+        photoAPartager.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(prendrePhoto))
+        photoAPartager.addGestureRecognizer(tap)
+        
+        texteAPartager.text = texteVide
+    }
+    
+    @objc func prendrePhoto() {
         
     }
+    
+    
+    
     
     @IBAction func boutonPartagerAppuye(_ sender: Any) {
     }
